ea1f7182f58b9294657029e10104d1d2897ade74
Hòa Nguyễn Văn <nvh0412@gmail.com>
2015-07-29 00:29:40 +0700

    [Intent] Extend Intent
    Try to call some activity by Implicit Intent.

--- IntentApplication/app/src/main/java/com/example/hoanv/intentapplication/MainActivity.java
+++ IntentApplication/app/src/main/java/com/example/hoanv/intentapplication/MainActivity.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends Activity {
@@ -20,18 +25,20 @@ public class MainActivity extends Activity {
 
         Button btnSearch = (Button) findViewById(R.id.btn_search);
         Button btnSend = (Button) findViewById(R.id.btn_send);
+        Button btnCalendar = (Button) findViewById(R.id.btn_calendar);
+        Button btnContact = (Button) findViewById(R.id.btn_contact);
         final EditText txtSearch = (EditText) findViewById(R.id.txt_location);
-//        btnSearch.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent();
-//                intent.setAction(Intent.ACTION_VIEW);
-//                intent.setData(Uri.parse("geo:0,0?q=" + txtSearch.getText().toString()));
-//                startActivity(intent);
-//            }
-//
-//        });
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:0,0?q=" + txtSearch.getText().toString()));
+                startActivity(intent);
+            }
+
+        });
 
         btnSend.setOnClickListener(new View.OnClickListener() {
 
@@ -39,8 +46,37 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_SENDTO);
                 intent.setData(Uri.parse("mailto:"));
-                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{ txtSearch.getText().toString() });
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{txtSearch.getText().toString()});
                 startActivity(intent);
+            }
+
+        });
+
+        btnCalendar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_INSERT);
+                intent.setData(CalendarContract.Events.CONTENT_URI);
+                intent.putExtra(CalendarContract.Events.TITLE, txtSearch.getText().toString());
+                intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, Calendar.getInstance());
+                intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, Calendar.getInstance());
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+
+        });
+
+        btnContact.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
             }
 
         });
@@ -66,5 +102,13 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Uri contactChoosen = data.getData();
+            Log.i("INTENT" , contactChoosen.toString());
+        }
     }
 }
